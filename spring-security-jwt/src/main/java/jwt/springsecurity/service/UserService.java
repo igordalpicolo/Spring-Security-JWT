c5f4b7174e29500ea25f3520924bdd6aa319d14c
Igor Dal Picolo <igordalpicolo11@hotmail.com>
2023-11-28 21:14:07 -0300

    Configurando classes user

---
+++ spring-security-jwt/src/main/java/jwt/springsecurity/service/UserService.java
@@ -0,0 +1,26 @@
+package jwt.springsecurity.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import jwt.springsecurity.model.User;
+import jwt.springsecurity.repository.UserRepository;
+
+@Service
+public class UserService {
+   
+	@Autowired
+    private UserRepository repository;
+   
+	@Autowired
+    private PasswordEncoder encoder;
+    public void createUser(User user){
+      
+    	String pass = user.getPassword();
+        
+    	//criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
